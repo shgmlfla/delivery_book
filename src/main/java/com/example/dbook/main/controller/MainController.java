52b8674d1e59ed3d715ac70add4770d9736a1f06
gmlfla <noeheelim@naver.com>
2026-03-25 09:51:42 +0900

    feat : 메인페이지 controller 추

--- src/main/java/com/example/dbook/main/controller/MainController.java
+++ src/main/java/com/example/dbook/main/controller/MainController.java
@@ -1,23 +1,24 @@
 package com.example.dbook.main.controller;
 
 import com.example.dbook.book.dto.BestSellerBookDto;
+import com.example.dbook.book.dto.SearchBookDto;
 import com.example.dbook.book.dto.HotTrendBookDto;
 import com.example.dbook.book.dto.NewBookDto;
 import com.example.dbook.book.service.BookApiService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class MainController {
 
     private final BookApiService bookApiService;
-
-    public MainController(BookApiService bookApiService) {
-        this.bookApiService = bookApiService;
-    }
 
     @GetMapping("/")
     public String main(Model model) {
@@ -34,6 +35,37 @@ public class MainController {
 
         return "main/mainPage";
     }
+
+    @GetMapping("/test")
+    public String mainTest(Model model) {
+        String searchDt = "2025-12-05";
+        String libCode = "141553";
+
+        List<HotTrendBookDto> hotTrendBookList = bookApiService.getHotTrendBooks(searchDt);
+        List<NewBookDto> newBookList = bookApiService.getNewBook(libCode);
+        List<BestSellerBookDto> bestSellerBookList = bookApiService.getBestSeller(searchDt);
+
+        model.addAttribute("hotTrendBooks", hotTrendBookList);
+        model.addAttribute("newBooks", newBookList);
+        model.addAttribute("bestSellerBooks", bestSellerBookList);
+
+        return "main/mainPageTest";
+    }
+
+    @GetMapping("/searchResults")
+    public String searchResults(@RequestParam(value="title", required = false) String title,
+                         @RequestParam(value="author", required = false) String author,
+                         Model model) {
+
+        List<SearchBookDto> searchBookResults = bookApiService.getSearchBook(title, author);
+
+        model.addAttribute("searchBookResults", searchBookResults);
+        model.addAttribute("title", title);
+        model.addAttribute("author", author);
+
+        return "main/searchResults";
+    }
+
 
 
 }
